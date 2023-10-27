b912ed821444b9aed0ee361582fc441346da375d
Don Kanaille <149098194+donkanaille42@users.noreply.github.com>
2023-10-27 19:17:41 +0200

    Add spring boot web app

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    id("com.diffplug.spotless") version "6.22.0"
+}
+
+group = "com.github.clubmanager1999"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+        ktlint()
+        licenseHeaderFile("${project.rootProject.projectDir}/HEADER")
+    }
+    kotlinGradle { ktlint() }
+    flexmark {
+        target("**/*.md")
+        flexmark()
+    }
+}
