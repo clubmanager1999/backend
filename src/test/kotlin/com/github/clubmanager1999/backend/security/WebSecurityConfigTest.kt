ac637a0ee285b4a657e85eeb359dc4036cd9cd2f
Don Kanaille <149098194+donkanaille42@users.noreply.github.com>
2023-11-21 23:16:02 +0100

    Check every endpoint against all permissions (#45)

--- src/test/kotlin/com/github/clubmanager1999/backend/security/WebSecurityConfigTest.kt
+++ src/test/kotlin/com/github/clubmanager1999/backend/security/WebSecurityConfigTest.kt
@@ -24,8 +24,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.test.util.AssertionErrors
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
@@ -50,131 +53,221 @@ class WebSecurityConfigTest {
         )
 
     private final val memberEndpoints =
-        listOf(
-            Endpoint("Get member by id", HttpMethod.GET, "/api/members/42"),
-            Endpoint("Get all members", HttpMethod.GET, "/api/members"),
-            Endpoint("Add member", HttpMethod.POST, "/api/members"),
-            Endpoint("Update member", HttpMethod.PUT, "/api/members/42"),
-            Endpoint("Delete member", HttpMethod.DELETE, "/api/members/42"),
+        Group(
+            permission = Permission.MANAGE_MEMBERS,
+            endpoints =
+                listOf(
+                    Endpoint("Get member by id", HttpMethod.GET, "/api/members/42"),
+                    Endpoint("Get all members", HttpMethod.GET, "/api/members"),
+                    Endpoint("Add member", HttpMethod.POST, "/api/members"),
+                    Endpoint("Update member", HttpMethod.PUT, "/api/members/42"),
+                    Endpoint("Delete member", HttpMethod.DELETE, "/api/members/42"),
+                ),
         )
 
     private final val membershipEndpoints =
-        listOf(
-            Endpoint("Get membership by id", HttpMethod.GET, "/api/memberships/43"),
-            Endpoint("Get all memberships", HttpMethod.GET, "/api/memberships"),
-            Endpoint("Add membership", HttpMethod.POST, "/api/memberships"),
-            Endpoint("Update membership", HttpMethod.PUT, "/api/memberships/43"),
-            Endpoint("Delete membership", HttpMethod.DELETE, "/api/memberships/43"),
+        Group(
+            permission = Permission.MANAGE_MEMBERSHIPS,
+            endpoints =
+                listOf(
+                    Endpoint("Get membership by id", HttpMethod.GET, "/api/memberships/43"),
+                    Endpoint("Get all memberships", HttpMethod.GET, "/api/memberships"),
+                    Endpoint("Add membership", HttpMethod.POST, "/api/memberships"),
+                    Endpoint("Update membership", HttpMethod.PUT, "/api/memberships/43"),
+                    Endpoint("Delete membership", HttpMethod.DELETE, "/api/memberships/43"),
+                ),
         )
 
     private final val roleEndpoints =
-        listOf(
-            Endpoint("Get role by name", HttpMethod.GET, "/api/roles/foo"),
-            Endpoint("Get all roles", HttpMethod.GET, "/api/roles"),
-            Endpoint("Add role", HttpMethod.POST, "/api/roles"),
-            Endpoint("Add permission", HttpMethod.POST, "/api/roles/foo/permissions"),
-            Endpoint("Delete permission", HttpMethod.DELETE, "/api/roles/foo/permissions/foo"),
-            Endpoint("Delete role", HttpMethod.DELETE, "/api/roles/foo"),
-            Endpoint("Add role to member", HttpMethod.POST, "/api/members/42/roles"),
-            Endpoint("Remove role from member", HttpMethod.DELETE, "/api/members/42/roles/foo"),
+        Group(
+            permission = Permission.MANAGE_ROLES,
+            endpoints =
+                listOf(
+                    Endpoint("Get role by name", HttpMethod.GET, "/api/roles/foo"),
+                    Endpoint("Get all roles", HttpMethod.GET, "/api/roles"),
+                    Endpoint("Add role", HttpMethod.POST, "/api/roles"),
+                    Endpoint("Update role", HttpMethod.PUT, "/api/roles/42"),
+                    Endpoint("Delete role", HttpMethod.DELETE, "/api/roles/foo"),
+                    Endpoint("Add permission", HttpMethod.POST, "/api/roles/foo/permissions"),
+                    Endpoint("Delete permission", HttpMethod.DELETE, "/api/roles/foo/permissions/foo"),
+                    Endpoint("Add role to member", HttpMethod.PUT, "/api/roles/42/holder"),
+                    Endpoint("Remove role from member", HttpMethod.DELETE, "/api/roles/42/holder"),
+                ),
         )
 
     private final val transactionEndpoints =
-        listOf(
-            Endpoint("Get transaction by id", HttpMethod.GET, "/api/transactions/44"),
-            Endpoint("Get all transactions", HttpMethod.GET, "/api/transactions"),
-            Endpoint("Add transaction", HttpMethod.POST, "/api/transactions"),
-            Endpoint("Update transaction", HttpMethod.PUT, "/api/transactions/44"),
-            Endpoint("Delete transaction", HttpMethod.DELETE, "/api/transactions/44"),
+        Group(
+            permission = Permission.MANAGE_TRANSACTIONS,
+            endpoints =
+                listOf(
+                    Endpoint("Get transaction by id", HttpMethod.GET, "/api/transactions/44"),
+                    Endpoint("Get all transactions", HttpMethod.GET, "/api/transactions"),
+                    Endpoint("Add transaction", HttpMethod.POST, "/api/transactions"),
+                    Endpoint("Update transaction", HttpMethod.PUT, "/api/transactions/44"),
+                    Endpoint("Delete transaction", HttpMethod.DELETE, "/api/transactions/44"),
+                ),
         )
 
     private final val donorEndpoints =
-        listOf(
-            Endpoint("Get donor by id", HttpMethod.GET, "/api/donors/45"),
-            Endpoint("Get all donors", HttpMethod.GET, "/api/donors"),
-            Endpoint("Add donor", HttpMethod.POST, "/api/donors"),
-            Endpoint("Update donor", HttpMethod.PUT, "/api/donors/45"),
-            Endpoint("Delete donor", HttpMethod.DELETE, "/api/donors/45"),
+        Group(
+            permission = Permission.MANAGE_DONORS,
+            endpoints =
+                listOf(
+                    Endpoint("Get donor by id", HttpMethod.GET, "/api/donors/45"),
+                    Endpoint("Get all donors", HttpMethod.GET, "/api/donors"),
+                    Endpoint("Add donor", HttpMethod.POST, "/api/donors"),
+                    Endpoint("Update donor", HttpMethod.PUT, "/api/donors/45"),
+                    Endpoint("Delete donor", HttpMethod.DELETE, "/api/donors/45"),
+                ),
         )
 
     private final val creditorEndpoints =
-        listOf(
-            Endpoint("Get creditor by id", HttpMethod.GET, "/api/creditors/45"),
-            Endpoint("Get all creditors", HttpMethod.GET, "/api/creditors"),
-            Endpoint("Add creditor", HttpMethod.POST, "/api/creditors"),
-            Endpoint("Update creditor", HttpMethod.PUT, "/api/creditors/45"),
-            Endpoint("Delete creditor", HttpMethod.DELETE, "/api/creditors/45"),
+        Group(
+            permission = Permission.MANAGE_CREDITORS,
+            endpoints =
+                listOf(
+                    Endpoint("Get creditor by id", HttpMethod.GET, "/api/creditors/45"),
+                    Endpoint("Get all creditors", HttpMethod.GET, "/api/creditors"),
+                    Endpoint("Add creditor", HttpMethod.POST, "/api/creditors"),
+                    Endpoint("Update creditor", HttpMethod.PUT, "/api/creditors/45"),
+                    Endpoint("Delete creditor", HttpMethod.DELETE, "/api/creditors/45"),
+                ),
         )
 
     private final val receiptEndpoints =
-        listOf(
-            Endpoint("Get receipt by id", HttpMethod.GET, "/api/receipts/45"),
-            Endpoint("Get all receipts", HttpMethod.GET, "/api/receipts"),
-            Endpoint("Add receipt", HttpMethod.POST, "/api/receipts"),
-            Endpoint("Update receipt", HttpMethod.PUT, "/api/receipts/45"),
-            Endpoint("Delete receipt", HttpMethod.DELETE, "/api/receipts/45"),
+        Group(
+            permission = Permission.MANAGE_RECEIPTS,
+            endpoints =
+                listOf(
+                    Endpoint("Get receipt by id", HttpMethod.GET, "/api/receipts/45"),
+                    Endpoint("Get all receipts", HttpMethod.GET, "/api/receipts"),
+                    Endpoint("Add receipt", HttpMethod.POST, "/api/receipts"),
+                    Endpoint("Update receipt", HttpMethod.PUT, "/api/receipts/45"),
+                    Endpoint("Delete receipt", HttpMethod.DELETE, "/api/receipts/45"),
+                ),
         )
 
     private final val mappingEndpoints =
-        listOf(
-            Endpoint("Get mapping by id", HttpMethod.GET, "/api/mappings/45"),
-            Endpoint("Get all mappings", HttpMethod.GET, "/api/mappings"),
-            Endpoint("Add mapping", HttpMethod.POST, "/api/mappings"),
-            Endpoint("Update mapping", HttpMethod.PUT, "/api/mappings/45"),
-            Endpoint("Delete mapping", HttpMethod.DELETE, "/api/mappings/45"),
+        Group(
+            permission = Permission.MANAGE_MAPPINGS,
+            endpoints =
+                listOf(
+                    Endpoint("Get mapping by id", HttpMethod.GET, "/api/mappings/45"),
+                    Endpoint("Get all mappings", HttpMethod.GET, "/api/mappings"),
+                    Endpoint("Add mapping", HttpMethod.POST, "/api/mappings"),
+                    Endpoint("Update mapping", HttpMethod.PUT, "/api/mappings/45"),
+                    Endpoint("Delete mapping", HttpMethod.DELETE, "/api/mappings/45"),
+                ),
         )
 
     private final val purposeEndpoints =
-        listOf(
-            Endpoint("Get purpose by id", HttpMethod.GET, "/api/purposes/45"),
-            Endpoint("Get all purposes", HttpMethod.GET, "/api/purposes"),
-            Endpoint("Add purpose", HttpMethod.POST, "/api/purposes"),
-            Endpoint("Update purpose", HttpMethod.PUT, "/api/purposes/45"),
-            Endpoint("Delete purpose", HttpMethod.DELETE, "/api/purposes/45"),
+        Group(
+            permission = Permission.MANAGE_PURPOSES,
+            endpoints =
+                listOf(
+                    Endpoint("Get purpose by id", HttpMethod.GET, "/api/purposes/45"),
+                    Endpoint("Get all purposes", HttpMethod.GET, "/api/purposes"),
+                    Endpoint("Add purpose", HttpMethod.POST, "/api/purposes"),
+                    Endpoint("Update purpose", HttpMethod.PUT, "/api/purposes/45"),
+                    Endpoint("Delete purpose", HttpMethod.DELETE, "/api/purposes/45"),
+                ),
         )
 
     private final val areaEndpoints =
-        listOf(
-            Endpoint("Get area by id", HttpMethod.GET, "/api/areas/45"),
-            Endpoint("Get all areas", HttpMethod.GET, "/api/areas"),
-            Endpoint("Add area", HttpMethod.POST, "/api/areas"),
-            Endpoint("Update area", HttpMethod.PUT, "/api/areas/45"),
-            Endpoint("Delete area", HttpMethod.DELETE, "/api/areas/45"),
+        Group(
+            permission = Permission.MANAGE_AREAS,
+            endpoints =
+                listOf(
+                    Endpoint("Get area by id", HttpMethod.GET, "/api/areas/45"),
+                    Endpoint("Get all areas", HttpMethod.GET, "/api/areas"),
+                    Endpoint("Add area", HttpMethod.POST, "/api/areas"),
+                    Endpoint("Update area", HttpMethod.PUT, "/api/areas/45"),
+                    Endpoint("Delete area", HttpMethod.DELETE, "/api/areas/45"),
+                ),
         )
 
     private final val electionEndpoints =
-        listOf(
-            Endpoint("Get all elections", HttpMethod.GET, "/api/elections"),
+        Group(
+            permission = Permission.MANAGE_ROLES,
+            endpoints =
+                listOf(
+                    Endpoint("Get all elections", HttpMethod.GET, "/api/elections"),
+                ),
         )
 
     private final val adminEndpoints =
-        memberEndpoints + membershipEndpoints + roleEndpoints + transactionEndpoints + donorEndpoints +
-            creditorEndpoints + receiptEndpoints + mappingEndpoints + purposeEndpoints + areaEndpoints +
-            electionEndpoints
+        listOf(
+            memberEndpoints, membershipEndpoints, roleEndpoints, transactionEndpoints, donorEndpoints,
+            creditorEndpoints, receiptEndpoints, mappingEndpoints, purposeEndpoints, areaEndpoints,
+            electionEndpoints,
+        )
 
-    private final val allEndpoints = invalidEndpoints + userEndpoints + adminEndpoints
+    private final val allEndpoints = adminEndpoints.flatMap { it.endpoints } + invalidEndpoints + userEndpoints
 
     @TestFactory
-    fun shouldReturnUnauthorizedOnMissingToken(): List<DynamicTest> {
-        return allEndpoints.map {
-            DynamicTest.dynamicTest(it.name) {
-                mockMvc
-                    .perform(MockMvcRequestBuilders.request(it.method, it.url))
-                    .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+    fun shouldReturnUnauthorizedOnMissingJwt(): List<DynamicTest> {
+        return allEndpoints
+            .map { endpoint ->
+                DynamicTest.dynamicTest(endpoint.name) {
+                    mockMvc
+                        .perform(MockMvcRequestBuilders.request(endpoint.method, endpoint.url))
+                        .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+                }
+            }
+    }
+
+    @TestFactory
+    fun shouldReturnForbiddenOnMissingPermission(): List<DynamicTest> {
+        return adminEndpoints
+            .flatMap { group -> group.endpoints }
+            .map { endpoint ->
+                DynamicTest.dynamicTest(endpoint.name) {
+                    mockMvc
+                        .perform(MockMvcRequestBuilders.request(endpoint.method, endpoint.url).withoutRole())
+                        .andExpect(MockMvcResultMatchers.status().isForbidden)
+                }
+            }
+    }
+
+    @TestFactory
+    fun shouldReturnForbiddenOnWrongPermission(): List<DynamicTest> {
+        return adminEndpoints
+            .flatMap { group ->
+                group.endpoints
+                    .flatMap { endpoint ->
+                        Permission.entries
+                            .filter { it != group.permission }
+                            .map { permission ->
+                                DynamicTest.dynamicTest("${endpoint.name} ($permission)") {
+                                    mockMvc
+                                        .perform(MockMvcRequestBuilders.request(endpoint.method, endpoint.url).withRole(permission))
+                                        .andExpect(MockMvcResultMatchers.status().isForbidden)
+                                }
+                            }
+                    }
+            }
+    }
+
+    @TestFactory
+    fun shouldNotReturnForbiddenOnCorrectPermission(): List<DynamicTest> {
+        return adminEndpoints.flatMap { group ->
+            group.endpoints.map { endpoint ->
+                DynamicTest.dynamicTest("${endpoint.name} (${group.permission}") {
+                    mockMvc
+                        .perform(MockMvcRequestBuilders.request(endpoint.method, endpoint.url).withRole(group.permission))
+                        .andExpect { result: MvcResult ->
+                            AssertionErrors.assertNotEquals(
+                                "Status",
+                                HttpStatus.FORBIDDEN,
+                                result.response.status,
+                            )
+                        }
+                }
             }
         }
     }
 
-    @TestFactory
-    fun shouldReturnForbiddenOnMissingRole(): List<DynamicTest> {
-        return adminEndpoints.map {
-            DynamicTest.dynamicTest(it.name) {
-                mockMvc
-                    .perform(MockMvcRequestBuilders.request(it.method, it.url).withoutRole())
-                    .andExpect(MockMvcResultMatchers.status().isForbidden)
-            }
-        }
-    }
+    data class Group(val permission: Permission, val endpoints: List<Endpoint>)
 
     data class Endpoint(val name: String, val method: HttpMethod, val url: String)
 }
